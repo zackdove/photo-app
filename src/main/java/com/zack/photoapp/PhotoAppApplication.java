aa1e5e0fdb72f48b556b5c4418c8afd09efc0682
zackdove <zdove@live.co.uk>
2019-06-14 14:42:36 +0100

    Initialisation

---
+++ src/main/java/com/zack/photoapp/PhotoAppApplication.java
@@ -0,0 +1,13 @@
+package com.zack.photoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApplication.class, args);
+	}
+
+}
