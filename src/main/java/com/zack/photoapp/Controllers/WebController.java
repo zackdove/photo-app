7da8fc6c667319b3e98e582d39c5d3ced1c5fd17
zackdove <zdove@live.co.uk>
2019-06-14 17:41:34 +0100

    trying to configure webjars

--- src/main/java/com/zack/photoapp/Controllers/WebController.java
+++ src/main/java/com/zack/photoapp/Controllers/WebController.java
@@ -2,6 +2,8 @@ package com.zack.photoapp.Controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
@@ -9,8 +11,12 @@ public class WebController {
 
     private static final Logger LOG = LoggerFactory.getLogger(WebController.class);
 
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index");
+
+
+    @GetMapping("/")
+    public String addUser(Model model){
+        LOG.info("Get request for index");
+        return "index";
     }
 
 }
---
+++ src/main/resources/webjars/typicons-master/README.md
@@ -0,0 +1,5 @@
+#WebJar for typicons
+
+More info: http://webjars.org
+
+Upstream: https://github.com/stephenhutchings/typicons.font/
