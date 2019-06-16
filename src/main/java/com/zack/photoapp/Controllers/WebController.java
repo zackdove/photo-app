6eb1bcc662ed4d07ab27b921f185103e65eafbf9
zackdove <zdove@live.co.uk>
2019-06-16 22:19:19 +0100

    Can now retrieve images from the AWS S3 Bucket

--- src/main/java/com/zack/photoapp/Controllers/WebController.java
+++ src/main/java/com/zack/photoapp/Controllers/WebController.java
@@ -1,21 +1,28 @@
 package com.zack.photoapp.Controllers;
 
+import com.zack.photoapp.Services.AmazonS3Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
+@Controller
 public class WebController {
 
     private static final Logger LOG = LoggerFactory.getLogger(WebController.class);
 
+    @Autowired
+    private AmazonS3Service amazonS3Service;
 
 
     @GetMapping("/")
     public String addUser(Model model){
         LOG.info("Get request for index");
+        amazonS3Service.getPictures();
         return "index";
     }
 
---
+++ src/main/java/com/zack/photoapp/Services/AmazonS3Service.java
@@ -0,0 +1,56 @@
+package com.zack.photoapp.Services;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.opencsv.CSVReader;
+import com.zack.photoapp.Controllers.WebController;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+@Service
+public class AmazonS3Service {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WebController.class);
+
+    private void objectToFile(S3Object s3object) {
+        try {
+            S3ObjectInputStream inputStream = s3object.getObjectContent();
+            String filename = "src/main/resources/static/images/" + s3object.getKey();
+            FileUtils.copyInputStreamToFile(inputStream, new File(filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getPictures(){
+        //Looks in ~/.aws/credentials for credentials
+        AmazonS3 s3client = AmazonS3ClientBuilder
+                .standard()
+                .withRegion(Regions.US_EAST_1)
+                .build();
+//        ObjectListing objectListing = s3client.listObjects("zack-photo-app-bucket");
+//        for(S3ObjectSummary os : objectListing.getObjectSummaries()) {
+//            LOG.info(os.getKey());
+//
+//        }
+        S3Object img1 = s3client.getObject("zack-photo-app-bucket","000001bw.jpg");
+        objectToFile(img1);
+
+
+    }
+}
