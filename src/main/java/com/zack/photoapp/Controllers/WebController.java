2bb2777ed533bebb949f863d05a2ec86dd5785da
zackdove <zdove@live.co.uk>
2019-06-17 16:53:40 +0100

    Adding new index page

--- src/main/java/com/zack/photoapp/Controllers/WebController.java
+++ src/main/java/com/zack/photoapp/Controllers/WebController.java
@@ -1,6 +1,5 @@
 package com.zack.photoapp.Controllers;
 
-import com.zack.photoapp.Services.AmazonS3Service;
 import com.zack.photoapp.Services.PhotoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @Controller
 public class WebController {
@@ -19,12 +16,17 @@ public class WebController {
     @Autowired
     private PhotoService photoService;
 
-
     @GetMapping("/")
-    public String addUser(Model model){
-        LOG.info("Get request for index");
-        model.addAttribute("photos", photoService.getFirst10());
+    public String getIndex(Model model){
+        LOG.info("Get request for /");
         return "index";
+    }
+
+    @GetMapping("/photographs")
+    public String getPhotographs(Model model){
+        LOG.info("Get request for /photographs");
+        model.addAttribute("photos", photoService.getFirst10());
+        return "photographs";
     }
 
 }
