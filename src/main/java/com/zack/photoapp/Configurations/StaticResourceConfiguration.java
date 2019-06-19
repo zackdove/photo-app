f503be89c74c247f7750b8bcb510e7bf4f008dae
zackdove <zdove@live.co.uk>
2019-06-19 15:01:07 +0100

    more config messing around

--- src/main/java/com/zack/photoapp/Configurations/StaticResourceConfiguration.java
+++ src/main/java/com/zack/photoapp/Configurations/StaticResourceConfiguration.java
@@ -3,12 +3,13 @@ package com.zack.photoapp.Configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
+public class StaticResourceConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/**").addResourceLocations("file:/var/images/");
+		registry.addResourceHandler("/var/images/**").addResourceLocations("file:/var/images/");
 	}
 }
